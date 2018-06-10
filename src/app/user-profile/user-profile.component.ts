fae7db8aa2b4ae8c9a25570f7360cfc6620c6d92
Anton Isakov <isakovanton@mail.ru>
2018-06-11 02:06:32 +0400

    UserProfile Component is rebuilded with the new Data Service

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 
-import { findUserById, findUserByProperty } from '../../db/users';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'trp-user-profile',
@@ -9,14 +9,65 @@ import { findUserById, findUserByProperty } from '../../db/users';
 })
 export class UserProfileComponent implements OnInit {
   @Input() lkActive: boolean;
-  objectKeys = Object.keys;
+  // objectKeys = Object.keys;
 
-  user = findUserById(2);
+  // user = findUserById(2);
   // getLkVisibility = getLkVisibility;
 
-  constructor() { }
+  users;
+  places;
+
+  user;
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.dataService.getUsers()
+      .subscribe((data) => {
+        this.users = data;
+        console.log('Users:', this.users);
+    });
+
+    this.dataService.getPlaces()
+      .subscribe((data) => {
+        this.places = data;
+        console.log('Places:', this.places);
+    });
+
+    this.dataService.getUser('ivan')
+      .subscribe((data) => {
+        this.user = data;
+        console.log('User:', this.user);
+    });
+
+    const user = {
+      "login": "petr",
+      "password": "1111",
+      "name": "Петр Петров",
+      "info": {
+        "age": 24,
+        "sex": "male"
+      },
+      "places": {
+        "restrict": [
+          "Kazan",
+        ],
+        "fav": [
+          "Moscow",
+        ]
+      },
+      "preferences": [
+        {
+          "architecture": 10,
+          "water":        40,
+          "modern":      100,
+          "people":       10
+        }
+      ]
+    };
+
+    // this.dataService.addUser(user)
+    //   .subscribe(user => console.warn(user));
   }
 
 }
