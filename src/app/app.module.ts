286d58051daf2536df400c05fba357007a34e137
Anton Isakov <isakovanton@mail.ru>
2018-06-11 02:05:40 +0400

    HttpClentModule is imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -18,6 +19,8 @@ import { MatDividerModule } from '@angular/material/divider';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+
     MatSliderModule,
     MatDividerModule
   ],
