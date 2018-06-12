8ee214e7f86e9ea278979083dc90aab5cad427b6
Anton Isakov <isakovanton@mail.ru>
2018-06-13 00:58:24 +0400

    Server mock for Stackblitz

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -17,7 +17,7 @@ export class UserProfileComponent implements OnInit {
   users;
   places;
 
-  user;
+  user: any = users[0];
 
   constructor(private dataService: DataService) { }
 
@@ -50,5 +50,139 @@ export class UserProfileComponent implements OnInit {
         console.log('Updated user', this.user);
       });
   }
-
 }
+
+var users = [
+  {
+    "login": "ivan",
+    "password": "0000",
+    "name": "Иван",
+    "info": [
+      {
+        "name": "Возраст",
+        "value": "22"
+      },
+      {
+        "name": "Пол",
+        "value": "Мужской"
+      }
+    ],
+    "places": {
+      "restrict": [
+        "Ufa",
+        "Lipetzk"
+      ],
+      "fav": [
+        "Kazan",
+        "Ekaterinburg"
+      ]
+    },
+    "preferences": [
+      {
+        "id": "history",
+        "name": "История",
+        "value": 9
+      },
+      {
+        "id": "architecture",
+        "name": "Ахитектура",
+        "value": 9
+      },
+      {
+        "id": "nature",
+        "name": "Природа",
+        "value": 4
+      },
+      {
+        "id": "food",
+        "name": "Еда",
+        "value": 7
+      },
+      {
+        "id": "modern",
+        "name": "Модерн",
+        "value": 10
+      },
+      {
+        "id": "culture",
+        "name": "Культура",
+        "value": 7
+      },
+      {
+        "id": "water",
+        "name": "Пляжный отдых",
+        "value": 1
+      },
+      {
+        "id": "active",
+        "name": "Активный отдых",
+        "value": 8
+      }
+    ]
+  },
+  {
+    "login": "petr",
+    "password": "1111",
+    "name": "Петр",
+    "info": [
+      {
+        "name": "Возраст",
+        "value": "24"
+      },
+      {
+        "name": "Пол",
+        "value": "Мужской"
+      }
+    ],
+    "places": {
+      "restrict": [
+        "Kazan"
+      ],
+      "fav": [
+        "Moscow"
+      ]
+    },
+    "preferences": [
+      {
+        "id": "history",
+        "name": "История",
+        "value": 1
+      },
+      {
+        "id": "architecture",
+        "name": "Ахитектура",
+        "value": 7
+      },
+      {
+        "id": "nature",
+        "name": "Природа",
+        "value": 9
+      },
+      {
+        "id": "food",
+        "name": "Еда",
+        "value": 5
+      },
+      {
+        "id": "modern",
+        "name": "Модерн",
+        "value": 7
+      },
+      {
+        "id": "culture",
+        "name": "Культура",
+        "value": 3
+      },
+      {
+        "id": "water",
+        "name": "Пляжный отдых",
+        "value": 10
+      },
+      {
+        "id": "active",
+        "name": "Активный отдых",
+        "value": 10
+      }
+    ]
+  }
+];
